ef4ae415bd28438b934bf0ed28e13e22c85e9ab2
maclm <mantoine.mac@gmail.com>
2021-03-30 11:01:43 -0400

    modif template

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -37,7 +37,7 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
                     {/* The main header section on top of the screen */}
                     <header className="site-head" style={{ ...site.cover_image && { backgroundImage: `url(${site.cover_image})` } }}>
                         <div className="container">
-                            <div className="site-mast">
+                            {/* <div className="site-mast">
                                 <div className="site-mast-left">
                                     <Link to="/">
                                         {site.logo ?
@@ -51,7 +51,7 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
                                     { site.facebook && <a href={ facebookUrl } className="site-nav-item" target="_blank" rel="noopener noreferrer"><img className="site-nav-icon" src="/images/icons/facebook.svg" alt="Facebook" /></a>}
                                     <a className="site-nav-item" href={ `https://feedly.com/i/subscription/feed/${config.siteUrl}/rss/` } target="_blank" rel="noopener noreferrer"><img className="site-nav-icon" src="/images/icons/rss.svg" alt="RSS Feed" /></a>
                                 </div>
-                            </div>
+                            </div> */}
                             { isHome ?
                                 <div className="site-banner">
                                     <h1 className="site-banner-title">{site.title}</h1>
@@ -61,10 +61,10 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
                             <nav className="site-nav">
                                 <div className="site-nav-left">
                                     {/* The navigation items as setup in Ghost */}
-                                    <Navigation data={site.navigation} navClass="site-nav-item" />
+                                    {/* <Navigation data={site.navigation} navClass="site-nav-item" /> */}
                                 </div>
                                 <div className="site-nav-right">
-                                    <Link className="site-nav-button" to="/about">About</Link>
+                                    {/* <Link className="site-nav-button" to="/about">About</Link> */}
                                 </div>
                             </nav>
                         </div>
@@ -82,10 +82,13 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
                     <footer className="site-foot">
                         <div className="site-foot-nav container">
                             <div className="site-foot-nav-left">
-                                <Link to="/">{site.title}</Link> © 2021 &mdash; Published with <a className="site-foot-nav-item" href="https://ghost.org" target="_blank" rel="noopener noreferrer">Ghost</a>
+                                {/* <Link to="/">{site.title}</Link> © 2021 &mdash; Published with <a className="site-foot-nav-item" href="https://ghost.org" target="_blank" rel="noopener noreferrer">Ghost</a> */}
+                            </div>
+                            <div className="site-foot-nav-center">
+                                <a className="site-foot-nav-item" href="https://www.museedecharlevoix.qc.ca/" target="_blank" rel="noopener noreferrer">Musée de Charlevoiux</a>
                             </div>
                             <div className="site-foot-nav-right">
-                                <Navigation data={site.navigation} navClass="site-foot-nav-item" />
+                                {/* <Navigation data={site.navigation} navClass="site-foot-nav-item" /> */}
                             </div>
                         </div>
                     </footer>
