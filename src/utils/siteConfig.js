0f346cb2c770c90aee0567a962d53a3fedc80bb0
maclm <mantoine.mac@gmail.com>
2021-05-16 19:09:25 -0400

    Update siteConfig.js
    corrigé siteUrl

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: `expo.gatsbyjs.io`, // Site domain. Do not include a trailing slash!
+    siteUrl: `http://expo.gatsbyjs.io`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
